4db52d18c0dfb2c7228301541c679bd3341e50df
Sipos Miklós <commit@siposm.hu>
2021-11-10 09:58:30 +0100

    blogcontroller added

--- BlogSystem/BlogSystem.Endpoint/Startup.cs
+++ BlogSystem/BlogSystem.Endpoint/Startup.cs
@@ -14,6 +14,7 @@ namespace BlogSystem.Endpoint
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers(); // enable controllers
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
